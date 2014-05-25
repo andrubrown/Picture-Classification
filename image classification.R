2d02afdaf37f55801cf05385a4b4ab1787af54d9
Arif <arifyali@berkeley.edugit config --global user.email arifyali@berkeley.edu>
2014-05-24 17:10:47 -0700

    Final additions to this repo
    The project has been completed.

--- image classification.R
+++ image classification.R
@@ -4,7 +4,7 @@ img = read.csv("train.csv", header = F)
 names(img) = c("V1", as.character(1:512))
 img$V1 = as.factor(img$V1)
 
-test.rows = sample(1:dim(img)[1], 300)
+test.rows = sample(1:dim(img)[1], 700)
 test.rows = test.rows[order(test.rows)]
 training.sample = img[test.rows, ]
 testing.sample = img[-test.rows, -1]
@@ -17,23 +17,23 @@ test = predicted.img.class == img[-test.rows, 1]
 names(test) = img[-test.rows, 1]
 test
 mean(test)
-
+#[1] 0.86
 1 - mean(test[img[-test.rows, 1]==8])
-#[1] 0.109375
+#[1] 0.1111111
 1 - mean(test[img[-test.rows, 1]==7])
-#[1] 0.08928571
+#[1] 0
 1 - mean(test[img[-test.rows, 1]==6])
-#[1] 0.5671642
+#[1] 0.5714286
 1 - mean(test[img[-test.rows, 1]==5])
-#[1] 0.09090909
+#[1] 0.2
 1 - mean(test[img[-test.rows, 1]==4])
-#[1] 0.1578947
+#[1] 0.1666667
 1 - mean(test[img[-test.rows, 1]==3])
-#[1] 0.2058824
+#[1] 0.1428571
 1 - mean(test[img[-test.rows, 1]==2])
-#[1] 0.1323529
+#[1] 0
 1 - mean(test[img[-test.rows, 1]==1])
-#[1] 0.1846154
+#[1] 0.09090909
 #beat Matt 1 4 5 & 7, so method will be favored in this case
 #[1] 0.802 this is the overall prediction accuracy based on created test data
 
@@ -46,5 +46,5 @@ names(test_img) = as.character(1:512)
 img.svm = svm(V1~., data = train_img, type = "C-classification")
 predicted.img.class = predict(img.svm, test_img)
 
-write.csv(data.frame(id = c(1:length(predicted.img.class)),Predictions =  predicted.img.class), file = "predictions.cs")
+write.csv(data.frame(Predictions =  predicted.img.class), file = "predictions.csv")
 #0.76589 was the actual accuracy went tested against the Kaggle data
